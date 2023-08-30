ec5210f4071187c5514d3a34259a1d6fb65932e2
Vibhor Bhatt <108974586+vibhorbhatt777@users.noreply.github.com>
2023-08-30 13:33:12 +0530

    Shuffle the Array.cpp
    Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
    
    Return the array in the form [x1,y1,x2,y2,...,xn,yn].
    
     
    
    Example 1:
    
    Input: nums = [2,5,1,3,4,7], n = 3
    Output: [2,3,5,4,1,7] 
    Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
    Example 2:
    
    Input: nums = [1,2,3,4,4,3,2,1], n = 4
    Output: [1,4,2,3,3,2,4,1]
    Example 3:
    
    Input: nums = [1,1,2,2], n = 2
    Output: [1,2,1,2]
     
    
    Constraints:
    
    1 <= n <= 500
    nums.length == 2n
    1 <= nums[i] <= 10^3

---
+++ Shuffle the Array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        for(int i= 0 ;i<nums.size();i++){
+            int currNum = nums[i];
+            int currPos = i;
+            while(currNum>0){
+                int newPos{};
+                if(currPos< n){
+                    newPos = 2*currPos;
+                }else{
+                    newPos = 2 * (currPos - n) + 1;
+                }
+                int newNum = nums[newPos];
+                nums[newPos] = -currNum;
+                currNum = newNum;
+                currPos = newPos;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] < 0){
+                nums[i]= -nums[i];
+                
+
+            }
+        }
+        return nums;
+
+    }
+};
